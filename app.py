83621ba187c705169a3fb6bf2303a1d14063b819
Peggy <peggy.souttre@fabeon.fr>
2024-11-27 14:04:45 +0100

    input + selectbox intitulé du poste ok

--- app.py
+++ app.py
@@ -14,48 +14,30 @@ df = pd.read_csv("./df_tech_clean3.csv", delimiter = ',', encoding='utf-8')
 ##########################################################################################################################
 
 
-
 # Création du input pour le poste
 input_intitule = st.sidebar.text_input ('Intitulé du poste')
 if input_intitule :
-    df = df[df['intitule'].str.contains(input_intitule, case=False, na=False)]       # Filtrer les données en fonction de l'intitulé du poste sans tenir compte de la casse   
+    filtered_df = df[df['intitule'].str.contains(input_intitule, case=False, na=False)]       # Filtrer les données en fonction de l'intitulé du poste sans tenir compte de la casse   
    
-    if df.empty :
+    if filtered_df.empty :
         st.error(f"Ce poste n'existe pas dans la base")
     else: 
         print("Entrée input poste valide") 
 else: 
-    print("Marque filtrée")
+    filtered_df = df
 
 # Filtre par Titre de Poste 
 titre_poste = st.sidebar.selectbox ( 
     'Sélectionner le titre de poste', 
-    options=df['intitule'].unique(), 
+    options=['Tous'] + list(filtered_df['intitule'].unique()), 
     index = 0
 ) 
-# Convertir titre_poste en liste
-#titre_poste_list = [titre_poste] if titre_poste else []
+# Appliquer un second filtre si un intitulé spécifique est sélectionné
+if titre_poste != 'Tous':
+    final_df = filtered_df[filtered_df['intitule'] == titre_poste]
+else:
+    final_df = filtered_df  # Si 'Tous' est sélectionné, afficher tous les postes filtrés
 
-#if titre_poste_list :
-#   df = df[df['intitule'].isin(titre_poste_list)]
-
-# Filtre par Localisation 
-#localisation = st.sidebar.multiselect( 
-#   'Sélectionner la localisation', 
-#    options=df['lieuTravail_libelle'].unique(), 
-#   default=df['lieuTravail_libelle'].unique() 
-#) 
-
-# Filtre par Entreprise 
-#entreprise = st.sidebar.multiselect( 
-#   'Sélectionner l\'entreprise', 
-#    options=df['entreprise_nom'].unique(), 
- #   default=df['entreprise_nom'].unique() ) 
-
-# Appliquer les filtres aux données 
-#filtered_df = df[ (df['intitule'].isin(titre_poste_list)) 
-                 #& (df['lieuTravail_libelle'].isin(localisation)) & (df['entreprise_nom'].isin(entreprise)) 
-#                 ] 
 
 # Afficher les données filtrées 
-st.write('Données Filtrées', df)
+st.write('Données Filtrées', final_df)
