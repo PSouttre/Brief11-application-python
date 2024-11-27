78fa6e9737e52f22483fc0dc0cf2ac50907ae9af
Peggy <peggy.souttre@fabeon.fr>
2024-11-27 12:26:59 +0100

    input intitulé du poste

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 st.title("Offres d'emploi")
 
 # Récupération des données depuis un fichier csv
-df = pd.read_csv("./df_tech_clean.csv", delimiter = ',', encoding='utf-8')
+df = pd.read_csv("./df_tech_clean3.csv", delimiter = ',', encoding='utf-8')
 
 
 ##########################################################################################################################
@@ -15,17 +15,47 @@ df = pd.read_csv("./df_tech_clean.csv", delimiter = ',', encoding='utf-8')
 
 
 
+# Création du input pour le poste
+input_intitule = st.sidebar.text_input ('Intitulé du poste')
+if input_intitule :
+    df = df[df['intitule'].str.contains(input_intitule, case=False, na=False)]       # Filtrer les données en fonction de l'intitulé du poste sans tenir compte de la casse   
+   
+    if df.empty :
+        st.error(f"Ce poste n'existe pas dans la base")
+    else: 
+        print("Entrée input poste valide") 
+else: 
+    print("Marque filtrée")
+
 # Filtre par Titre de Poste 
-titre_poste = st.sidebar.multiselect( 'Sélectionner le titre de poste', options=df['intitule'].unique(), default=df['intitule'].unique() ) 
+titre_poste = st.sidebar.selectbox ( 
+    'Sélectionner le titre de poste', 
+    options=df['intitule'].unique(), 
+    index = 0
+) 
+# Convertir titre_poste en liste
+#titre_poste_list = [titre_poste] if titre_poste else []
+
+#if titre_poste_list :
+#   df = df[df['intitule'].isin(titre_poste_list)]
 
 # Filtre par Localisation 
-localisation = st.sidebar.multiselect( 'Sélectionner la localisation', options=df['lieuTravail_libelle'].unique(), default=df['lieuTravail_libelle'].unique() ) 
+#localisation = st.sidebar.multiselect( 
+#   'Sélectionner la localisation', 
+#    options=df['lieuTravail_libelle'].unique(), 
+#   default=df['lieuTravail_libelle'].unique() 
+#) 
 
 # Filtre par Entreprise 
-entreprise = st.sidebar.multiselect( 'Sélectionner l\'entreprise', options=df['entreprise_nom'].unique(), default=df['entreprise_nom'].unique() ) 
+#entreprise = st.sidebar.multiselect( 
+#   'Sélectionner l\'entreprise', 
+#    options=df['entreprise_nom'].unique(), 
+ #   default=df['entreprise_nom'].unique() ) 
 
 # Appliquer les filtres aux données 
-filtered_df = df[ (df['intitule'].isin(titre_poste)) & (df['lieuTravail_libelle'].isin(localisation)) & (df['entreprise_nom'].isin(entreprise)) ] 
+#filtered_df = df[ (df['intitule'].isin(titre_poste_list)) 
+                 #& (df['lieuTravail_libelle'].isin(localisation)) & (df['entreprise_nom'].isin(entreprise)) 
+#                 ] 
 
 # Afficher les données filtrées 
-st.write('Données Filtrées', filtered_df)
+st.write('Données Filtrées', df)
